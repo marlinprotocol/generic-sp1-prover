e0de6c621e569e4122f4d171c1c1c3aa92fd3027
Akshay | Marlin <akshay_111meher@hotmail.com>
2024-07-05 12:48:48 +0400

    Update run_container.sh

--- run_container.sh
+++ run_container.sh
@@ -6,5 +6,5 @@ docker rm -f `docker ps -aq`
 docker rmi $(docker images -f "dangling=true" -q)
 docker rm -f kalypso-sp1-prover
 
-docker run -d --name kalypso-sp1-prover sp1-prover
+docker run -d --name kalypso-sp1-prover --restart always sp1-prover
 docker logs kalypso-sp1-prover -f
