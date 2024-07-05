4d9ec96dfc7e6cb3a89bfd23e83ff634f18799f3
akshay <akshay_111meher@hotmail.com>
2024-07-05 14:07:06 +0400

    update

--- run_container.sh
+++ run_container.sh
@@ -6,5 +6,5 @@ docker rm -f `docker ps -aq`
 docker rmi $(docker images -f "dangling=true" -q)
 docker rm -f kalypso-sp1-prover
 
-docker run -d --name kalypso-sp1-prover --restart always sp1-prover
-docker logs kalypso-sp1-prover -f
+docker run -d --name market-20 --restart always sp1-prover
+docker logs market-20 -f
